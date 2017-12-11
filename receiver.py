21a1925e4ed4c0e1d3f32525ad6a16df5d0623ca
exvn4697 <ervin_4697@yahoo.com>
2017-12-11 11:08:28 +0800

    refactoring_2

--- receiver.py
+++ receiver.py
@@ -45,16 +45,19 @@ def check_delay(t, now):
     return t
 
 def update_x(t):
+    global x
     x = x + [t]
     if( len(x) > n):
         x = x[1:]
     
 def update_y(data):
+    global y
     y = y + [data]
     if( len(y) >n ):
         y = y[1:]
         
-def update_freq(x):
+def update_freq():
+    global freq
     if len(x)>=n :
         freq = n/(x[n-1]-x[0])
         
@@ -114,7 +117,6 @@ while True:
         x_poly = np.linspace(min(x),max(x),n*100)
         y_poly = np.polyval(coefs,x_poly )
         axes1.plot(x_poly ,y_poly,color='green', linewidth= 1, linestyle ='-')
-        #axes1.set_xlim( min(x),max(x) )
         axes1.relim()
         axes1.autoscale_view()
         
@@ -122,23 +124,24 @@ while True:
         if(len(x) >= n):
           yff = np.fft.fft(y)
           yf = np.fft.fft(y)/n
-          yf = yf[range(n//2)]
-          #xf = np.linspace(0.0,1.0/(2.0/800.0),n//2)
-          xf = (np.arange(n)/n*freq)[range(n//2)]
           
+          #update real fft
+          yf = yf[range(n//2)]
+          xf = (np.arange(n)/n*freq)[range(n//2)]
           axes[1].lines[0].remove()
           axes[1].plot(xf,abs(yf),'b',linewidth=1)
           axes[1].set_ylim(-1.0,1.0)
           
+          #update filtered fft
           ydel = yf
           ydel[0:wn] = 0.0
           axes[1].lines[0].remove()
           axes[1].plot(xf,abs(ydel),'r',linewidth=1)
+          
           #update inverse fft
-          axes1.lines[0].remove()
           yff[wn:-wn] = 0.0
           yff = np.fft.ifft(yff)
-          #yff = np.fft.ifft(np.append(yff[:3], [0.0]*(n-3)))
+          axes1.lines[0].remove()
           axes1.plot(x,yff,color='blue',linewidth=1)
         else:
           axes1.lines[0].remove()
